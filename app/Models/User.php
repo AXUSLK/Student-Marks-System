9ec6aa1321a77a418d0e741d9a632f50b9209ad6
AXUSLK <thilina.lyceum@gmail.com>
2023-05-20 10:14:37 +0530

    📦️ Auth Added Laravel Breeze
    Version: 1.0.0
    📦️ Laravel Breeze added
    🗃️ Database improvements
    🌱 Admin user seeder added
    🌱 Roles seeder added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,8 +19,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'dob',
         'password',
     ];
 
@@ -39,7 +42,13 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'dob' => 'datetime',
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function getNameAttribute()
+    {
+        return ($this->first_name . ' ' . $this->last_name);
+    }
 }
